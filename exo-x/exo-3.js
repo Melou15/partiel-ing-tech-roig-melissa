d565f3c4b66ba0b0a68d0f71b0e83aa8c2a72506
Melissa Roig <melissa.roig@laposte.net>
2023-04-20 10:51:22 +0200

    tableau des avions+ affichage du modele

---
+++ exo-x/exo-3.js
@@ -0,0 +1,19 @@
+/*Exo-3 Dans un fichier js (Reprendre le code de la classe Avion)
+Écrire un code qui permet de créer un tableau 
+qui va stocker 5 avions et afficher leur nom*/
+
+/*class Avion {
+  constructeur(Marque, Modele, longueur, nbPlace, Reservoir, vitesse)*/
+
+let zavions = [];
+
+
+zavions.push(new Avion("Michel", "lAugustin", 56, 200, 12000, 700));
+zavions.push(new Avion("Jose", "leMoustachu", 54, 300, 12000, 835));
+zavions.push(new Avion("Patrick", "leCampeur", 87, 250, 12000, 923));
+zavions.push(new Avion("JM", "lePilier", 93, 200, 12000, 534));
+zavions.push(new Avion("Francois", "leFrancais", 83, 100, 12000, 279));
+
+for (let i = 0; i < zavions.length; i++) {
+  console.log(zavions[i].Modele);
+}
